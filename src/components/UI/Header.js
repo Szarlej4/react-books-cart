6498e969de1b688cf5fd91eb43bf44f1dcc18f5c
szarlej <jakub.szarleja@gmail.com>
2023-02-08 19:30:27 +0100

    Changed font to roboto, added Header component with styling

---
+++ src/components/UI/Header.js
@@ -0,0 +1,40 @@
+import styles from "./Header.module.css";
+import library from "../../assets/images/library-background.jpg";
+
+const Header = () => {
+	return (
+		<header>
+			<section className={styles.nav}>
+				<div className={styles.nav__wrapper}>
+					<a href="/" className={styles.nav__logo}>
+						Book Rental
+					</a>
+					{/* <Card /> */}
+				</div>
+			</section>
+			<div className={styles.imgWrapper}>
+				<img
+					src={library}
+					alt="library"
+					className={styles.backgroundImg}
+				/>
+			</div>
+			<div className={styles.desc}>
+				<p className={styles.desc__title}>
+					Best books, always available
+				</p>
+				<p className={styles.desc__desc}>
+					Choose your favorite book from our broad selection of
+					available books and enjoy a great time reading in our place
+					or at home.
+				</p>
+				<p className={styles.desc__desc}>
+					All our books are chosen by book charts of best selling
+					books, each year, annualy, we are always open for you.
+				</p>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
